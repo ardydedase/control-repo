6f6ac550ca1162ca055ae81ba5f992f7a49a4873
Ardy Dedase <ardy.dedase@gmail.com>
2019-06-30 11:01:49 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,10 @@
 node default {
     file { '/root/README.md':
         ensure  => file,
-        content => 'This is a readme',
+        content => 'This should exist',
         owner   => 'root',
     }
+    file { '/root/README.md':
+        owner   => 'root',
+    }            
 }
