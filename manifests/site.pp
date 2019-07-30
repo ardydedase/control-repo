f88fe2301d85f2788d556953c86f0493aa9e3a37
Ardy Dedase <ardy.dedase@gmail.com>
2019-07-30 20:47:31 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,15 +4,9 @@ node default {
         content => 'This should exist',
         owner   => 'root',
     }
-    file { '/root/README.md':
-        owner   => 'root',
-    }
     file { '/tmp/README.md':
         ensure  => file,
         content => 'This should exist',
         owner   => 'root',
     }
-    file { '/tmp/README.md':
-        owner   => 'root',
-    }    
 }
